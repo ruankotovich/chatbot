d4115036f11ea318459ac274befa49383b862d4f
Erick R. Ribeiro <erick.ribeiro.16@gmail.com>
2019-09-08 22:28:10 -0400

    fix: integration Heroku

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,5 @@ echo "++++++++++ TRAINING MODELS ++++++++++"
 rasa train
 
 
-echo "++++++++++ EXTRACT CREDENTIALS ++++++++++"
-sh ./generate_credentials.sh > credentials.yml
-
-
 echo "++++++++++ STARTING SERVER ++++++++++"
-python -m rasa_core.run -d models/dialogue -u models/current/nlu  --port $PORT --credentials credentials.yml
+rasa run --port 5001 --credentials credentials.yml --endpoints endpoints.yml --debug
